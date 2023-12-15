ce0b870462f73bf4ec6f88fb24df28a03941c3a8
TongChia <tongchia@live.com>
2023-12-15 19:59:24 +0800

    ...;

--- service/start.sh
+++ service/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+addr=$(hostname -I | awk '{print $1}')
+addr6=$(ip -6 addr show scope global | grep inet6 | awk '{print $2}' | cut -d "/" -f 1)
+
 readonly regex="^(4|6|all),[0-9]+,[a-zA-Z0-9.-]+,[0-9]+$"
 
 grep -E "${regex}" < /etc/6tunnel/config | while IFS= read -r config
@@ -10,11 +13,11 @@ do
   TARGET_PORT=${args[3]}
 
   if [ "${args[0]}" == "4" ]; then
-    6tunnel -4 "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
+    6tunnel -l "${addr}" -4 "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
   elif [ "${args[0]}" == "6" ]; then
-    addr=$(ip -6 addr show scope global | grep inet6 | awk '{print $2}' | cut -d "/" -f 1)
-    6tunnel -l "${addr}" -6 "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
+    6tunnel -l "${addr6}" -6 "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
   elif [ "${args[0]}" == "all" ]; then
-    6tunnel "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
+    6tunnel -l "${addr}" -4 "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
+    6tunnel -l "${addr6}" -6 "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
   fi
 done
