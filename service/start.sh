b9b64785f2f057b252be2067233bf081d95b60f8
TongChia <tongchia@live.com>
2023-12-15 19:40:43 +0800

    fix args;

--- service/start.sh
+++ service/start.sh
@@ -13,8 +13,8 @@ do
   elif [ "${args[0]}" == "6" ]; then
     LOCAL_PORT_OPT="-6"
   fi
-  LOCAL_PORT=args[1]
-  TARGET_HOST=args[2]
-  TARGET_PORT=args[3]
+  LOCAL_PORT=${args[1]}
+  TARGET_HOST=${args[2]}
+  TARGET_PORT=${args[3]}
   6tunnel -l "${addr}" "${LOCAL_PORT_OPT}" "${LOCAL_PORT}" "${TARGET_HOST}" "${TARGET_PORT}"
 done
